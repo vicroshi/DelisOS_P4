3073e2ca1907080db48ff9dc7118e1626aed2509
vic <lambrostars@gmail.com>
2024-01-30 15:58:28 +0200

    set operations, fts demo, fixed dirent errors

--- modules/diff.c
+++ modules/diff.c
@@ -13,83 +13,48 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fts.h>
 int filter(const struct dirent* dir){
     return dir->d_name[0]!='.';
 }
 
 void cmp_ent(struct dirent** a, struct dirent**b, int a_len, int b_len, struct dirent*** intersection,struct dirent*** diffA,struct dirent*** diffB,int* i_len, int* dA_len, int* dB_len){
     int max = (a_len > b_len ? a_len : b_len);
-//    printf("max %d\n",max);
-//    for (int i = 0; i < a_len; i++) {
-//        printf("size:%ld %s %d %ld %ld %d\n", sizeof(struct dirent),a[i]->d_name,a[i]->d_type,a[i]->d_ino,a[i]->d_off,a[i]->d_reclen);
-//        free(a[i]);
-//    }
     *intersection = malloc(sizeof(struct dirent*) * max);
     *diffA = malloc(sizeof(struct dirent*) * a_len);
     *diffB = malloc(sizeof(struct dirent*) * b_len);
-//    for (int i = 0; i < max; i++) {
-//        printf("i:%d ",i);
-//        (*intersection)[i] = malloc(sizeof(struct dirent));
-//        if (i<a_len){
-//            printf("i:%d ",i);
-//            (*diffA)[i] = malloc(sizeof(struct dirent));
-//        }
-//        if(i<b_len){
-//
-//            (*diffB)[i] = malloc(sizeof(struct dirent));
-//        }
-//    }
-    putchar('\n');
     int i = 0, j = 0, k = 0, ii = 0, jj = 0;
     while (i < a_len && j < b_len){
-        printf("i=%d j=%d k=%d ii=%d jj=%d\n",i,j,k,ii,jj);
+//        printf("i=%d j=%d k=%d ii=%d jj=%d\n",i,j,k,ii,jj);
         if(strcmp(a[i]->d_name,b[j]->d_name)<0){
-//            printf("ii:%d ",ii);
-//            struct dirent da;
-//            memcpy((*(*diffA)[ii]).d_name,(*a[i]).d_name,strlen((*a[i]).d_name)+1);
-//            (*(*diffA)[ii]).d_reclen = (*a[i]).d_reclen;
-//            (*(*diffA)[ii]).d_ino = (*a[i]).d_ino;
-//            (*(*diffA)[ii]).d_off = (*a[i]).d_off;
-//            (*(*diffA)[ii]).d_type = (*a[i]).d_type;
             (*diffA)[ii] = malloc(a[i]->d_reclen);
-            memcpy((*diffA)[ii],a[i],a[i]->d_reclen);
-            ii++;
+            memcpy((*diffA)[ii++],a[i],a[i]->d_reclen);
             i++;
         }
         else if(strcmp(a[i]->d_name,b[j]->d_name)>0){
             (*diffB)[jj] = malloc(b[i]->d_reclen);
-            *(*diffB)[jj++] = *b[j];
+            memcpy((*diffB)[jj++],b[j],b[j]->d_reclen);
             j++;
         }
         else{
 //            *(*intersection)[k++] = *a[i];
             (*intersection)[k] = malloc(a[i]->d_reclen);
-            memcpy((*intersection)[k],a[i],a[i]->d_reclen);
-//            memcpy((*(*intersection)[k]).d_name,(*a[i]).d_name,strlen((*a[i]).d_name)+1);
-//            (*(*intersection)[k]).d_reclen = (*a[i]).d_reclen;
-//            (*(*intersection)[k]).d_ino = (*a[i]).d_ino;
-//            (*(*intersection)[k]).d_off = (*a[i]).d_off;
-//            (*(*intersection)[k]).d_type = (*a[i]).d_type;
-//            memcpy((*intersection)[k],a[i], sizeof(struct dirent),sizeof(a[i], sizstrlen(a[i], sizeof(struct diren+1t))
+            memcpy((*intersection)[k++],a[i],a[i]->d_reclen);
             i++;
             j++;
         }
     }
     for (; i < a_len; i++) {
-        printf("i=%d j=%d k=%d ii=%d jj=%d\n",i,j,k,ii,jj);
-        memcpy((*diffA)[ii],a[i],a[i]->d_reclen);
-//        *(*diffA)[ii++] = *a[i];
-//        memcpy((*(*diffA)[ii]).d_name,(*a[i]).d_name,strlen((*a[i]).d_name)+1);
-//        (*(*diffA)[ii]).d_reclen = (*a[i]).d_reclen;
-//        (*(*diffA)[ii]).d_ino = (*a[i]).d_ino;
-//        (*(*diffA)[ii]).d_off = (*a[i]).d_off;
-//        (*(*diffA)[ii]).d_type = (*a[i]).d_type;
-        ii++;
+//        printf("i=%d j=%d k=%d ii=%d jj=%d\n",i,j,k,ii,jj);
+        (*diffA)[ii] = malloc(a[i]->d_reclen);
+        memcpy((*diffA)[ii++],a[i],a[i]->d_reclen);
     }
     for (; j < b_len; j++) {
-        printf("i=%d j=%d k=%d ii=%d jj=%d\n",i,j,k,ii,jj);
-        *(*diffB)[jj++] = *b[j];
+//        printf("i=%d j=%d k=%d ii=%d jj=%d\n",i,j,k,ii,jj);
+        (*diffB)[jj] = malloc(b[i]->d_reclen);
+        memcpy((*diffB)[jj++],a[i],b[j]->d_reclen);
     }
+    //isws kai na mh xreiazetai kan den glittwneis xwros
 //    *diffA = realloc(*diffA,ii*sizeof(struct dirent*));
 //    *diffB = realloc(*diffB,jj*sizeof(struct dirent*));
 //    *intersection = realloc(*intersection,k*sizeof(struct dirent*));
@@ -98,13 +63,48 @@ void cmp_ent(struct dirent** a, struct dirent**b, int a_len, int b_len, struct d
     *dB_len = jj;
 }
 
+void print_and_free(struct dirent ** arr, int len, char* name){
+    printf("%s: ",name);
+    for (int i = 0; i < len; i++) {
+        printf("%s ",arr[i]->d_name);
+        free(arr[i]);
+    }
+    free(arr);
+    putchar('\n');
+
+}
+
+void print_fts(FTSENT* list){
+    FTSENT* tmp = list;
+    char* type;
+    
+    while(tmp){
+        switch (tmp->fts_info) {
+            case FTS_D:
+                type = "directory";
+                break;
+            case FTS_F:
+                type = "file";
+                break;
+            case FTS_SL:
+                type = "softlink";
+                break;
+            default:
+                type = "unknown";
+                break;
+        }
+        printf("name: %s, path: %s, access_path: %s, level: %d, type: %s\n",tmp->fts_name, tmp->fts_path,tmp->fts_accpath,tmp->fts_level,type);
+
+        tmp = tmp->fts_link;
+    }
+}
+
 char diff(char* dirA, char* dirB){
+    //dirent api
     struct dirent** entriesA;
     struct dirent** entriesB;
     int lenB = scandir(dirB,&entriesB,filter,alphasort);
     int lenA = scandir(dirA,&entriesA,filter,alphasort);
-    printf("lenA=%d lenB=%d",lenA,lenB);
-
     struct dirent** intersection;
     struct dirent** diffA;
     struct dirent** diffB;
@@ -113,44 +113,28 @@ char diff(char* dirA, char* dirB){
     int lenDiffB;
     cmp_ent(entriesA,entriesB,lenA,lenB,
             &intersection,&diffA,&diffB,&lenIntr,&lenDiffA,&lenDiffB);
-    printf("intersection: ");
-    for (int i = 0; i < lenIntr; i++) {
-        printf("%s ",intersection[i]->d_name);
-    }
-    putchar('\n');
-    printf("diffA: ");
-    for (int i = 0; i < lenDiffA; i++) {
-        printf("%s ",diffA[i]->d_name);
-    }
-    putchar('\n');
-    printf("diffB: ");
-    for (int i = 0; i < lenDiffB; i++) {
-        printf("%s ",diffB[i]->d_name);
-    }
-    putchar('\n');
-    for (int i = 0; i < lenB; i++) {
-        free(entriesB[i]);
-    }
-    free(entriesB);
-    for (int i = 0; i < lenA; i++) {
-//        printf("%s %d %ld %ld %d\n",entriesA[i]->d_name,entriesA[i]->d_type,entriesA[i]->d_ino,entriesA[i]->d_off,entriesA[i]->d_reclen);
-        free(entriesA[i]);
-    }
-    free(entriesA);
-//    for (int i = 0; i < lenDiffA; i++) {
-    for (int i = 0; i < lenA; i++) {
-        free(diffA[i]);
-    }
-    free(diffA);
-//    for (int i = 0; i < lenDiffB; i++) {
-    for (int i = 0; i < lenB; i++) {
-        free(diffB[i]);
-    }
-    free(diffB);
-    for (int i = 0; i < 6; i++) {
-        free(intersection[i]);
-    }
-    free(intersection);
+    print_and_free(entriesA,lenA,"dirA");
+    print_and_free(entriesB,lenB,"dirB");
+    print_and_free(diffA,lenDiffA,"diffA");
+    print_and_free(diffB,lenDiffB,"diffB");
+    print_and_free(intersection,lenIntr,"intersection");
+    //fts api
+    /*char* fts_arg[] = {
+            dirA,
+            dirB,
+            NULL
+    };
+    FTS* ftsA = fts_open(fts_arg,FTS_PHYSICAL,NULL);
+    FTSENT* e;
+    FTSENT* entriesA;
+    FTSENT* entryA;
+    while ((entryA = fts_read(ftsA))){
+        entriesA = fts_children(ftsA,0);
+        if (entriesA){
+            printf("folder:%s\n",entryA->fts_name);
+            print_fts(entriesA);
+        }
+    }*/
     return 1;
 }
 
