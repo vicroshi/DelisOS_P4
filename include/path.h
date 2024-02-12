24461226bd110cd336f0501cc05a0700f87522f2
vicroshi <mgcroshi@gmail.com>
2024-02-12 14:44:13 +0200

    gitignore change

---
+++ include/path.h
@@ -0,0 +1,8 @@
+//
+// Created by vic on 2/12/24.
+//
+
+#ifndef DELISOS_P4_PATH_H
+#define DELISOS_P4_PATH_H
+
+#endif //DELISOS_P4_PATH_H
---
+++ modules/path.c
@@ -0,0 +1,9 @@
+//
+// Created by vic on 2/12/24.
+//
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "path.h"
+
